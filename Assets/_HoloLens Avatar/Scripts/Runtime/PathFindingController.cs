e30ec720bb1db1a7c4d439981e3e60e370f7147f
PubuduS <pubudusupun@gmail.com>
2021-10-31 21:58:43 -0500

    Add more avatar movement.
    Avatar now move forward when player gets close.
    Avatar will now look at the player, when in idle.
    Change Doxygen Configurations.

--- Assets/_HoloLens Avatar/Scripts/Runtime/PathFindingController.cs
+++ Assets/_HoloLens Avatar/Scripts/Runtime/PathFindingController.cs
@@ -10,19 +10,29 @@ using UnityEngine.AI;
 public class PathFindingController : MonoBehaviour
 {
 
-    [SerializeField] private float m_Distance = 10f;
+    [SerializeField] private float m_Distance = 5f;
     [SerializeField, Range(0f, 90f)] private float m_MaxAngle = 90f;
 
     private NavMeshAgent m_Agent;
     private Rigidbody m_Rigidbody;
     private Vector3 m_LastPosition;
-    private Quaternion m_LastRotation;
+    private Quaternion m_LastRotation;    
 
     void Awake()
     {
         m_Agent = GetComponent<NavMeshAgent>();
-        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Rigidbody = GetComponent<Rigidbody>();        
         MoveAgent();
+    }
+
+    private void Update()
+    {
+        MoveWhenPlayerIsNear();
+
+        if(m_Agent.velocity.magnitude <= 0f)
+        {
+            LookAtPlayer();
+        }
     }
 
     // Enable update coroutine when object or component is active
@@ -80,10 +90,38 @@ public class PathFindingController : MonoBehaviour
     /// Move the agent to forward direction.
     /// </summary>
     void MoveAgent() 
-    {
+    {        
         m_Rigidbody.isKinematic = true;
         var direction = Camera.main.transform.TransformDirection(Vector3.forward);
         var targetPosition = Camera.main.transform.position + (direction.normalized * m_Distance);
         m_Agent.SetDestination(targetPosition);
     }
+
+    /// <summary>
+    /// The avatar will start to move when the player get closer to the avatar.
+    /// </summary>
+    private void MoveWhenPlayerIsNear()
+    {
+        float distance = Vector3.Distance(Camera.main.transform.position, m_Agent.transform.position);
+
+        if (distance < 2.0f)
+        {
+            MoveAgent();
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    /// <summary>
+    /// The avatar will look at player's direction when not moving.
+    /// </summary>
+    private void LookAtPlayer()
+    {       
+        var direction = (Camera.main.transform.position - m_Agent.transform.position).normalized;
+        var lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Gait_Training
 A HoloLens 2 based gait training application
+
+What to do if you have Lock Input to game view error?
+To slove this issue, Go to
+Windows -> Analysis -> Input Debugger -> Options -> Lock Input to GameView
