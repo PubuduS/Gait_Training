50480063f140c4c33b9c587d9013bee6d1882028
PubuduS <pubudusupun@gmail.com>
2022-01-29 18:28:04 -0600

    Add a Mocap noise patterns.
    Add Default, Pink and White(Random) noise
    patterns to the project.
    
    Created a new animation controller which change
    the pattern when the avatar is walking and when
    user selected 1 of the 3 noise patterns.
    
    Add functionality to the UI. Now User can select
    any noise pattern and avatar will mimic that gait
    pattern.
    
    Remark: When exporting the Mocap data retargeting quality
    was lowered and Spine3 was discarded. See more details
    in import message section under animation.

--- Assets/_HoloLens_Avatar/Scripts/Runtime/AvatarAnimationState.cs
+++ Assets/_HoloLens_Avatar/Scripts/Runtime/AvatarAnimationState.cs
@@ -16,6 +16,8 @@ public class AvatarAnimationState : MonoBehaviour
     /// Reference to the NavMesh Agent 
     private NavMeshAgent m_NavMesh;
 
+    private SetNoise m_SetNoise;
+
     /// <summary>
     /// Gets called when script is loaded
     /// cached the references to Animator and NavMesh agent
@@ -24,16 +26,21 @@ public class AvatarAnimationState : MonoBehaviour
     {
         m_Animator = GetComponent<Animator>();
         m_NavMesh = GetComponent<NavMeshAgent>();
+        m_SetNoise = GameObject.Find("NoiseController").GetComponent<SetNoise>();
     }
 
     /// <summary>
     /// Gets called per each frame
     /// If navmesh velocity magnitude is greater than 0 then play moving animation otherwise idle animation
+    /// In addition to that it will also set the  gait patterns choosen by the user.
     /// </summary>
     void Update() 
     {
         // if navmesh velocity magnitude is greater than 0 then play moving animation otherwise idle animation
         var playerMoving = m_NavMesh.velocity.magnitude > 0f;
-        m_Animator.SetBool("IsWalking", playerMoving);
+
+        m_Animator.SetBool( "IsWalking", playerMoving );
+        m_Animator.SetInteger( "NoiseLvl", (int)m_SetNoise.GetNoisePattern() );       
     }
+
 }
---
+++ Assets/_HoloLens_Avatar/Scripts/Runtime/SetNoise.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// This class is used to set noise pattern levels.
+/// When user press a button, it will change the pattern.
+/// </summary>
+public class SetNoise : MonoBehaviour
+{
+    /// Enum for noise levels 
+    public enum NoiseTypes{ DefaultNoise = 0, PinkNoise = 1, WhiteNoise = 2 };
+
+    /// This will hold the user's selected pattern.
+    private NoiseTypes m_PatternType = NoiseTypes.DefaultNoise;
+
+    /// This will show the user selection of the pattern
+    /// under the avatar in avatar panel.
+    [SerializeField] private TextMeshPro m_NoiseLable;
+
+    /// <summary>
+    /// Gets called from the button's OnClick event.
+    /// 0 is for deafult pattern
+    /// 1 is for Pink noise pattern.
+    /// 2 is for White noise(Random) pattern.
+    /// </summary>
+    public void SetNoisePattern(int pattern)
+    {
+        m_PatternType = (NoiseTypes)pattern;
+        SetPatternLable();
+    }
+
+    /// <summary>
+    /// Sets the label according to the user selection.
+    /// User can see what they selected in Avatar panel
+    /// under the avatar.
+    /// </summary>
+    private void SetPatternLable()
+    {
+        switch( m_PatternType )
+        {
+            case NoiseTypes.DefaultNoise:
+                m_NoiseLable.text = "Noise: Default";
+                break;
+
+            case NoiseTypes.PinkNoise:
+                m_NoiseLable.text = "Noise: Pink";
+                break;
+
+            case NoiseTypes.WhiteNoise:
+                m_NoiseLable.text = "Noise: White";
+                break;
+
+            default:
+                Debug.Log("Incorect Input. Can't Change Labele");
+                m_NoiseLable.text = "!!! Noise: Error !!!";
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Gettr to get the user's selection.
+    /// </summary>
+    public NoiseTypes GetNoisePattern()
+    {
+        return m_PatternType;
+    }
+}
