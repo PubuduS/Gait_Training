e89a40fdf062b9790daeab5eb90da788c743a544
PubuduS <pubudusupun@gmail.com>
2022-03-03 13:30:20 -0600

    Add the Noise function.
    Now user can choose 3 types of gait  patterns.
    Avatars will change the gait patter user have selected.

--- Assets/_HoloLens_Avatar/Scripts/Runtime/AvatarAnimationState.cs
+++ Assets/_HoloLens_Avatar/Scripts/Runtime/AvatarAnimationState.cs
@@ -8,13 +8,15 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class AvatarAnimationState : MonoBehaviour 
 {
-    
-    
+        
     /// Reference to the animator    
     private Animator m_Animator;
 
     /// Reference to the NavMesh Agent 
     private NavMeshAgent m_NavMesh;
+
+    /// Reference to the SetNoise Script 
+    private SetNoise m_SetNoise;
 
     /// <summary>
     /// Gets called when script is loaded
@@ -24,16 +26,19 @@ public class AvatarAnimationState : MonoBehaviour
     {
         m_Animator = GetComponent<Animator>();
         m_NavMesh = GetComponent<NavMeshAgent>();
+        m_SetNoise = GameObject.Find("NoiseController").GetComponent<SetNoise>();
     }
 
     /// <summary>
     /// Gets called per each frame
     /// If navmesh velocity magnitude is greater than 0 then play moving animation otherwise idle animation
+    /// In addition to that it will also set the  gait patterns choosen by the user.
     /// </summary>
     void Update() 
     {
         // if navmesh velocity magnitude is greater than 0 then play moving animation otherwise idle animation
         var playerMoving = m_NavMesh.velocity.magnitude > 0f;
         m_Animator.SetBool("IsWalking", playerMoving);
+        m_Animator.SetInteger("NoiseLvl", (int)m_SetNoise.GetNoisePattern());        
     }
 }
